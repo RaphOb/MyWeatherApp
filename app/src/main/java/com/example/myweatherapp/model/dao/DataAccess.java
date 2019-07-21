6eb643cfa029384d3d19942a68d01c64b40452f7
Raph <obadia_r@etna-alternance.net>
2019-07-21 10:55:48 +0200

    yes

--- app/src/main/java/com/example/myweatherapp/model/dao/DataAccess.java
+++ app/src/main/java/com/example/myweatherapp/model/dao/DataAccess.java
@@ -1,13 +1,23 @@
 package com.example.myweatherapp.model.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
 
 import com.example.myweatherapp.model.common.CityFav;
+
+import java.util.List;
 
 @Dao
 public interface DataAccess {
 
     @Insert
     public void addTown(CityFav cityFav);
+
+    @Query("select * from citifav")
+    public List<CityFav> getCity();
+
+    @Delete
+    public void delCity(CityFav cityFav);
 }
