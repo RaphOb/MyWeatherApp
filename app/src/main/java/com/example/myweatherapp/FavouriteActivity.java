b6398b37bd20b5e0fb2640bee0c5f1a7b8d8d06c
Raph <obadia_r@etna-alternance.net>
2019-08-28 11:16:02 +0200

    add delete and click listener to favoris

--- app/src/main/java/com/example/myweatherapp/FavouriteActivity.java
+++ app/src/main/java/com/example/myweatherapp/FavouriteActivity.java
@@ -3,8 +3,6 @@ package com.example.myweatherapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,9 +11,7 @@ import com.example.myweatherapp.model.common.CityFav;
 import com.example.myweatherapp.model.common.ListFavViewModel;
 import com.example.myweatherapp.others.FavListAdapter;
 
-import java.util.List;
-
-public class FavouriteActivity extends AppCompatActivity {
+public class FavouriteActivity extends AppCompatActivity implements FavListAdapter.OnDeleteClickListener {
 
     private FavListAdapter favListAdapter;
     private ListFavViewModel listFavViewModel;
@@ -26,12 +22,20 @@ public class FavouriteActivity extends AppCompatActivity {
         setContentView(R.layout.list_fav);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        favListAdapter = new FavListAdapter(this);
+        favListAdapter = new FavListAdapter(this, this);
         recyclerView.setAdapter(favListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         listFavViewModel = ViewModelProviders.of(this).get(ListFavViewModel.class);
 
         listFavViewModel.getAllFavs().observe(this, cityFavs -> favListAdapter.setFav(cityFavs));
+
+
+
+    }
+
+    @Override
+    public void OnDeleteClickListener(CityFav mCityFav) {
+        listFavViewModel.delete(mCityFav);
     }
 }
